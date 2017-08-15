4dd2c592aecabfda13f2269bfef363e9b1dac6c2
yogesh-pandey <yogeshpandey10oct@gmail.com>
2017-08-15 17:16:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one' ,function (req, res) {
-   res.sendFile("Article one requested and will be served here!!!!");
+   res.send("Article one requested and will be served here!!!!");
 });
 
 app.get('/article-two' ,function (req, res) {
-   res.sendFile("Article two requested and will be served here!!!!");
+   res.send("Article two requested and will be served here!!!!");
 });
 
 app.get('/article-three' ,function (req, res) {
-   res.sendFile("Article three requested and will be served here!!!!"); 
+   res.send("Article three requested and will be served here!!!!"); 
 });
 
 app.get('/ui/style.css', function (req, res) {
