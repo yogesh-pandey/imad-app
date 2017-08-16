54dfcbb1b8ebdcaabc897f137fa2fd87e03c7b96
yogesh-pandey <yogeshpandey10oct@gmail.com>
2017-08-16 10:02:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,18 +5,35 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne = {
-    title : 'Article one | Yogesh Pandey',
-    heading : 'Article -one',
-    date : 'Aug 15 2017',
-    content: `
-     <p>This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
-    </p>
-    <p>This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
-    </p>
-    <p>This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
-    </p>`
+var articles = {
+    'article-one':{
+        title : 'Article one | Yogesh Pandey',
+        heading : 'Article-one',
+        date : 'Aug 15 2017',
+        content: `
+         <p>This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
+        </p>
+        <p>This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
+        </p>
+        <p>This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
+        </p>`
+},
+    'article-two':{
+        title : 'Article Two | Yogesh Pandey',
+        heading : 'Article-Two',
+        date : 'Aug 20 2017',
+        content: `
+         <p>This is the content of my second article. </p>`
+    },
+    'article-three':{
+        title : 'Article Three | Yogesh Pandey',
+        heading : 'Article-Three',
+        date : 'Aug 25 2017',
+        content: `
+         <p>This is the content of my third article. </p>`
+    }
 };
+
 function createTemplate (data) {
     var title = data.title;
     var date = data.date;
@@ -50,21 +67,26 @@ function createTemplate (data) {
     `;
     return htmlTemplate;
 }
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one' ,function (req, res) {
-   res.send(createTemplate(articleOne));
+app.get('/:articleName' ,function (req, res) {
+    //articeName == article-one
+    //articles[articleName] == {} content object for artcle one 
+    var articleName = req.param.articleName;
+    res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two' ,function (req, res) {
+/*app.get('/article-two' ,function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three' ,function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
-});
+}); */
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
