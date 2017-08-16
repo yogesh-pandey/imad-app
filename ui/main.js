094ed5e8fcb23c787c9f18244d2184b43022a323
yogesh-pandey <yogeshpandey10oct@gmail.com>
2017-08-16 12:14:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 //change the text of the main-text div
 var element = document.getElementById('main-text');
 element.innerHTML=("On clicking name Doesn't Change");
+
+//move the image
+var img= document.getElementById('madi');
+img.onlcick=function(){
+    img.style.marginLeft = '100px';
+};
