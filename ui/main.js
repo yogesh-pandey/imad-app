8658b9585927cc30b32ea87924454a6519c46736
yogesh-pandey <yogeshpandey10oct@gmail.com>
2017-08-16 12:29:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,6 +12,6 @@ function moveRight(){
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick=function(){
-    var interval=setInterval(moveRight,100);
+    var interval=setInterval(moveRight,50);
    // img.style.marginLeft = '100px';
 };
