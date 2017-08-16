38c5330c2790950560016295ded75b954d27e990
yogesh-pandey <yogeshpandey10oct@gmail.com>
2017-08-16 10:29:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ function createTemplate (data) {
     var htmlTemplate =`
     <!DOCTYPE html>
         <head>
-            <title> $title </title>
+            <title> $(title) </title>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link href="/ui/style.css" rel="stylesheet" />
         </head>
