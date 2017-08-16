8c62f9c562ec85d1a5b268dd61462c4c1b2a683b
yogesh-pandey <yogeshpandey10oct@gmail.com>
2017-08-16 13:41:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,6 +73,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter =0;
+app.get('/counter',function (req,res) {
+    counter =counter +1;
+    res.send(counter.toString());
+});
 app.get('/:articleName' ,function (req, res) {
     //articeName == article-one
     //articles[articleName] == {} content object for artcle one 
