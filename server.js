f91ccc3baf8dee952b6641dec338a48623274999
yogesh-pandey <yogeshpandey10oct@gmail.com>
2017-08-16 16:29:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
+var names=[];
 app.get('/submit-name/:name', function(req,res){
     //Get the name from the request
     var name= re.params.name;
