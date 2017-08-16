cc30f0ccea2b25a15930a834b0b365f9cf72d131
yogesh-pandey <yogeshpandey10oct@gmail.com>
2017-08-16 10:03:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ function createTemplate (data) {
                 </div>
                 <hr/>
                 <h3>
-                    $heading
+                    $(heading)
                 </h3>
                 <div>
                     $(date)
